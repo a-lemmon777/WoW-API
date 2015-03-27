1ad251e3b734a690a441de6f45cddcbc0b1a7edd
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-26 22:10:15 -0500

    Wrote server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+/**
+ * Created by a.lemmon777 on 3/26/2015.
+ */
+"use strict";
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+// bodyParser will let us get data from a POST request
+app.use(bodyParser.urlencoded({"extended": true})); // why true?
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// Routes
+var router = express.Router();
+
+// test rout
+router.get("/", function(req, res) { //put this in a controller
+    res.json({"message": "Hello!"});
+});
+
+// register the routes
+app.use("/api", router);
+
+// Start the server
+app.listen(port);
+console.log("Check out port " + port);
