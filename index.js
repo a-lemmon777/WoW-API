d3d38ff6dd66bde19ee93c6169e552481c725bc2
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-28 11:49:12 -0500

    Can now send a response

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/', function(request, response) {
+   response.send("It's working!");
+});
+
 app.listen(app.get('port'), function() {
     console.log("API is running on port " + app.get('port'));
 });
