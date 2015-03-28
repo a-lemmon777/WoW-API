0599efe79aa2b84f53e66863ddc9b2d7d253a305
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-28 10:33:10 -0500

    Changed to single quotes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /**
  * Created by a.lemmon777 on 3/26/2015.
  */
-"use strict";
-var express = require("express");
+'use strict';
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 
 // bodyParser will let us get data from a POST request
 app.use(bodyParser.urlencoded({"extended": true})); // why true?
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 var port = 9000;
 
-var mongoose = require("mongoose");
+var mongoose = require('mongoose')
 
 // Routes
 var router = express.Router();
@@ -23,7 +23,7 @@ router.get("/", function(req, res) { //put this in a controller
 });
 
 // register the routes
-app.use("/api", router);
+app.use('/api', router);
 
 // Start the server
 app.listen(port);
