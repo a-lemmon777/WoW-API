3a991401915dadb230dce6dcb7646145f5095c7f
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-29 15:32:30 -0500

    Dumbed down app.js to try getting heroku to work

--- app.js
+++ app.js
@@ -5,24 +5,24 @@
 
 // Some base setup
 var express = require('express');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
 // Database connection uses temporary credentials
-mongoose.connect('mongodb://devUser:devPass@ds045531.mongolab.com:45531/heroku_app35323377');
+//mongoose.connect('mongodb://devUser:devPass@ds045531.mongolab.com:45531/heroku_app35323377');
 var app = express();
-
+app.set('port', 5000);
 // Incoming data parsing
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({"extended": true})); // For parsing application/x-www-form-urlencoded format
-app.use(bodyParser.json({"type": ["json", "js", "text/*"]})); // For parsing json, javascript, and text formats
-var multer = require('multer');
-// Would put any received files in the uploads directory
-app.use(multer({dest: './uploads/'})); // For parsing multipart/form-data
+//var bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({"extended": true})); // For parsing application/x-www-form-urlencoded format
+//app.use(bodyParser.json({"type": ["json", "js", "text/*"]})); // For parsing json, javascript, and text formats
+//var multer = require('multer');
+//// Would put any received files in the uploads directory
+//app.use(multer({dest: './uploads/'})); // For parsing multipart/form-data
 
 console.log("Starting up");
 
 //var Account = require('./api/account/account.model');
-app.set('port', 5000);
+
 
 //Routes
 //var router = express.Router();
@@ -52,7 +52,11 @@ app.set('port', 5000);
 
 // Register routes
 //app.use('/api', router);
-require('./routes')(app);
+//require('./routes')(app);
+
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+});
 
 // Begin listening
 app.listen(app.get('port'), function() {
