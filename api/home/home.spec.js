61ab6d7bde5bdf20cb10fad41519ad36c0465c12
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-29 18:36:19 -0500

    Added some testing setup

---
+++ api/home/home.spec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by a.lemmon777 on 3/29/2015.
+ */
+'use strict';
+
+describe('Testing home', function() {
+   it('true is true', function() {
+       expect(true).toBe(true);
+   });
+});
