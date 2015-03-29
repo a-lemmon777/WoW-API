391767e994035f5456dcbb2157936b2c322137de
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-29 15:50:23 -0500

    Fixed commenting mistake

--- app.js
+++ app.js
@@ -5,14 +5,14 @@
 
 // Some base setup
 var express = require('express');
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 // Database connection uses temporary credentials
 mongoose.connect('mongodb://devUser:devPass@ds045531.mongolab.com:45531/heroku_app35323377');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-Incoming data parsing
+//Incoming data parsing
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({"extended": true})); // For parsing application/x-www-form-urlencoded format
 app.use(bodyParser.json({"type": ["json", "js", "text/*"]})); // For parsing json, javascript, and text formats
