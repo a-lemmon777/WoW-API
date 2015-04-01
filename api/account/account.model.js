adfee6ce52b4a586909eaafde30d5288fcfdc41e
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-04-01 18:22:21 -0500

    Cleaned up account.model

--- api/account/account.model.js
+++ api/account/account.model.js
@@ -11,26 +11,17 @@ var AccountSchema = new Schema({
     //"characters":
 });
 
-if (!AccountSchema.options.toObject) AccountSchema.options.toObject = {}; // Make an options object if there isn't one
-AccountSchema.options.toObject.transform = function(doc, ret, options) {
-    ret.account_id = ret._id;
-    // remove these fields before returning the result
-    delete ret.__v;
-    delete ret._id;
-};
-
-//AccountSchema.statics.getAllAccounts = function(callback) {
-//    this.find().exec(function(err, accounts) {
-//        var transformedAccounts = accounts.map(AccountSchema.toClient);
-//        callback;
-//        return transformedAccounts;
-//    });
-//};
+AccountSchema.set('toObject', {
+    transform: function(doc, ret, options) {
+        ret.account_id = ret._id;
+        // remove these fields before returning the result
+        delete ret.__v;
+        delete ret._id;
+    }
+});
 
 AccountSchema.statics.toClient = function(account) {
     return account.toObject();
 };
-
-
 
 module.exports = mongoose.model('Account', AccountSchema);
