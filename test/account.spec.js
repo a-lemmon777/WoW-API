d270ccf86d5d252906b312944b65de4682da2409
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-04-01 09:03:55 -0500

    Improved account tests

--- test/account.spec.js
+++ test/account.spec.js
@@ -7,38 +7,46 @@ var request = require('supertest');
 var Account = require('../api/account/account.model');
 
 describe('GET /account', function() {
-    it('should get an object with an empty array if the database is empty', function(done) {
-        Account.remove({}, function(err, accounts) {
-            request(app)
-                .get('/account')
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .end(function (err, response) {
-                    if (err) return done(err);
-                    response.body.should.have.property("accounts");
-                    response.body.accounts.should.have.length(0);
-                    done();
-                });
+    beforeEach(function(done) {
+        Account.remove().exec().then(function () {
+            done();
         });
+    });
+
+    after(function(done) {
+        Account.remove().exec().then(function() {
+            done();
+        });
+    });
+
+    it('should get an object with an empty array if the database is empty', function(done) {
+        request(app)
+            .get('/account')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, response) {
+                if (err) return done(err);
+                response.body.should.have.property("accounts");
+                response.body.accounts.should.have.length(0);
+                done();
+            });
     });
 
     it('should get an object with an array of length 2 if the database has 2 accounts', function(done) {
         var account1 = new Account({account_name: "TeamTed"});
         var account2 = new Account({account_name: "TeamTheresa"});
-        Account.remove({}, function() {
-            account1.save(function() {
-                account2.save(function() {
-                    request(app)
-                        .get('/account')
-                        .expect(200)
-                        .expect('Content-Type', /json/)
-                        .end(function (err, response) {
-                            if (err) return done(err);
-                            response.body.should.have.property("accounts");
-                            response.body.accounts.should.have.length(2);
-                            done();
-                        });
-                });
+        account1.save(function() {
+            account2.save(function() {
+                request(app)
+                    .get('/account')
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .end(function (err, response) {
+                        if (err) return done(err);
+                        response.body.should.have.property("accounts");
+                        response.body.accounts.should.have.length(2);
+                        done();
+                    });
             });
         });
     });
