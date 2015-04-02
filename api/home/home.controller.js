445eb083cef3fb1a32c847c79ed79e84e63dc4ff
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-04-02 08:53:44 -0500

    Expanded the welcome message

--- api/home/home.controller.js
+++ api/home/home.controller.js
@@ -4,5 +4,16 @@
 'use strict';
 
 exports.displayGreeting = function(request, response) {
-    response.send("Welcome to the World of Warcraft API!");
+    response.send("Welcome to the World of Warcraft API!<br>" +
+    "Things you can do:<br>" +
+    "=============================<br>" +
+    "GET {url}/about<br>" +
+    "POST {url}/account {'name': 'myAccount'}<br>" +
+    "GET {url}/account<br>" +
+    "POST {url}/account/{account_name}/characters {'name': 'Blackhand', 'race': 'Orc', 'class': 'Warrior', 'faction': 'Horde', 'level': 33}<br>" +
+    "DELETE {url}/account/{account_name}<br>" +
+    "DELETE {url}/account/{account_name}/characters/{character_name}<br>" +
+    "GET {url}/account/{account_name}/characters<br>" +
+    "PUT {url}/account/{account_name}/characters/{character_name} (used for undeleting a character)<br>" +
+    "Enjoy!<br>");
 };
